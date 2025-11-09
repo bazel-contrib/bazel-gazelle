bazel-contrib/bazel-gazelle

commit 0e862c3d81827441407c1e7c421831af511acfdb

perf: populate walk cache in background (#2161)
Is there any reason to block waiting on this? We can most likely start
`visit()`ing directories in parallel while the cache population is done
async in the background...

Normally the cache population will always be a step (or many steps)
ahead of `visit()` and this change should only make the 2 traversals
overlap but introduce no change to who fills the cache or what order it
is filled. The only scenario I can think of where `populateCache()`
would no longer be the first to fill the cache would be if a language
callback invokes `GetDirInfo`?

**What type of PR is this?**

Other

**What package or component does this PR mostly affect?**

all

**What does this PR do? Why is it needed?**

**Which issues(s) does this PR fix?**

**Other notes for review**

walk/walk.go
@@ -379,7 +379,8 @@ func newWalker(c *config.Config, cexts []config.Configurer, dirs []string, mode 
 		relsToVisitSeen: make(map[string]struct{}),
 	}
 
-	w.populateCache()
+	// Asynchronously populate the walker cache in the background.
+	go w.populateCache()
 
 	return w, nil
 }

commit 82d34d01250b4ac8efd3dce7806fdd59bc7ce810

fix: data race when populate walk cache in background
This data race has been introduced in
0e862c3d81827441407c1e7c421831af511acfdb (#2161).

walk/walk.go
@@ -25,6 +25,7 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/bazelbuild/bazel-gazelle/config"
 	"github.com/bazelbuild/bazel-gazelle/pathtools"
@@ -222,7 +223,10 @@ func Walk2(c *config.Config, cexts []config.Configurer, dirs []string, mode Mode
 
 	// Visit additional directories that extensions requested for indexing.
 	// Don't visit subdirectories recursively, even when recursion is enabled.
+	w.l.Lock()
 	w.mode = UpdateDirsMode
+	w.l.Unlock()
+
 	for len(w.relsToVisit) > 0 {
 		// Don't simply range over relsToVisit. We may append more.
 		relToVisit := w.relsToVisit[0]
@@ -262,6 +266,8 @@ func Walk2(c *config.Config, cexts []config.Configurer, dirs []string, mode Mode
 
 // walker holds state needed for a walk of the source tree.
 type walker struct {
+	l sync.RWMutex
+
 	// repoRoot is the absolute file path to the repo's root directory.
 	repoRoot string
 
@@ -383,6 +389,9 @@ func newWalker(c *config.Config, cexts []config.Configurer, dirs []string, mode 
 // We always need to visit directories requested by the caller and their
 // parents. We may also need to visit subdirectories.
 func (w *walker) shouldVisit(rel string, updateParent bool) bool {
+	w.l.RLock()
+	defer w.l.RUnlock()
+
 	switch w.mode {
 	case VisitAllUpdateSubdirsMode, VisitAllUpdateDirsMode:
 		return true
@@ -399,9 +408,17 @@ func (w *walker) shouldVisit(rel string, updateParent bool) bool {
 // parameter in the directory rel. This indicates the build file should be
 // updated.
 func (w *walker) shouldUpdate(rel string, updateParent bool) bool {
-	if (w.mode == VisitAllUpdateSubdirsMode || w.mode == UpdateSubdirsMode) && updateParent {
-		return true
+	if updateParent {
+		w.l.RLock()
+		defer w.l.RUnlock()
+
+		switch w.mode {
+		case VisitAllUpdateSubdirsMode, UpdateSubdirsMode:
+			return true
+		default:
+		}
 	}
+
 	return w.shouldUpdateRel[rel]
 }
 