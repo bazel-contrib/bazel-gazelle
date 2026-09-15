c0c336450bdc15ebb1d0fed13f1e3173956488aa
Jay Conrod <jay@engflow.com>
2026-09-15 16:43:20 -0700

    v2/cmd/gazelle/update: attach language and kind metadata to rules
    So that we can call merge, index, and resolve using the same
    extension that generated a rule.
    
    This is a very subtle change in behavior, but it's unlikely to affect
    anything in the real world, so this applies to both v1 and v2.
    Technically this allows multiple extensions to generate rules of
    the same kind, which was previously buggy and ambiguous but is now
    better defined.
    
    When indexing existing rules, Imports is still called on the last extension
    that declared support via Kinds() (no change in behavior here).
    
    Also plumbed through context to resolve.RuleIndex.AddRule and supported
    error handling.
    
    Fixes #2421
    Fixes #2280

---
+++ internal/language/test_same_kind_2/lang.go
@@ -0,0 +1,78 @@
+// Language test_same_kind_2 is used in //tests:same_kind to verify that
+// two extensions can generate rules with the same kind name.
+// See tests/same_kind/README.md.
+package test_same_kind_2
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bazel-contrib/bazel-gazelle/v2/label"
+	"github.com/bazel-contrib/bazel-gazelle/v2/language"
+	"github.com/bazel-contrib/bazel-gazelle/v2/resolve"
+	"github.com/bazel-contrib/bazel-gazelle/v2/rule"
+)
+
+func NewV2() language.Language {
+	return lang{}
+}
+
+type lang struct{}
+
+var _ language.Language = lang{}
+var _ language.Generator = lang{}
+var _ resolve.Indexer = lang{}
+var _ resolve.Resolver = lang{}
+
+const langName = "test_same_kind_2"
+
+func (lang) Name() string {
+	return langName
+}
+
+var kinds = []rule.KindInfo{
+	{
+		Name:           "same_kind",
+		LoadedFrom:     label.New("", "", "same_kind_2.bzl"),
+		MatchAttrs:     []string{"match_attr_2"},
+		MergeableAttrs: map[string]bool{"merge_attr_2": true},
+		ResolveAttrs:   map[string]bool{"resolve_attr_2": true},
+	},
+}
+
+func (lang) Kinds() []rule.KindInfo {
+	return kinds
+}
+
+func (lang) Generate(ctx context.Context, args language.GenerateArgs) (language.GenerateResult, error) {
+	r := rule.NewRule("same_kind", "r2")
+	r.SetAttr("match_attr_2", "match_r2")
+	r.SetAttr("merge_attr_2", "merge_r2")
+	return language.GenerateResult{
+		Gen:     []*rule.Rule{r},
+		Imports: []any{nil},
+	}, nil
+}
+
+func (lang) Imports(ctx context.Context, args resolve.ImportsArgs) (resolve.ImportsResult, error) {
+	r := args.Rule
+	match := r.AttrString("match_attr_2")
+	if match == "" {
+		return resolve.ImportsResult{}, fmt.Errorf("Imports called unexpectedly on rule %s without match_attr_2", r.Name())
+	}
+	return resolve.ImportsResult{
+		Imports: []resolve.ImportSpec{{
+			Lang: langName,
+			Imp:  match,
+		}},
+	}, nil
+}
+
+func (lang) Resolve(ctx context.Context, args resolve.ResolveArgs) error {
+	r := args.Rule
+	if r.AttrString("match_attr_2") == "" {
+		return fmt.Errorf("Resolve called unexpectedly on rule %s without match_attr_2", r.Name())
+	}
+	r.SetAttr("resolve_attr_2", "resolve_r2")
+	return nil
+}
